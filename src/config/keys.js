5800d174085e42791ab0ec1c9bb3874f73165231
Krzysztof Piekarz <kpiekarz1983@gmail.com>
2018-07-18 19:40:09 +0200

    3 - User API Routes & JWT Authentication

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://tmu_user:tmu_1234@ds129051.mlab.com:29051/social_tmu'
+  mongoURI: 'mongodb://tmu_user:tmu_1234@ds129051.mlab.com:29051/social_tmu',
+  secretOrKey: 'secret'
 };
